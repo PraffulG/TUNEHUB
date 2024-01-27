959deea5c8e1b14ccdbc6f291a5d26058f567dac
prafful <praffulgmpp@gmail.com>
2024-01-27 14:58:35 +0530

    First commit

--- src/main/java/com/example/demo/controller/UsersController.java
+++ src/main/java/com/example/demo/controller/UsersController.java
@@ -1,23 +1,34 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.entities.LoginData;
+import com.example.demo.entities.Song;
 import com.example.demo.entities.Users;
+import com.example.demo.services.SongService;
 import com.example.demo.services.UsersService;
 
 import jakarta.servlet.http.HttpSession;
-
-import org.springframework.web.bind.annotation.GetMapping;
+import okhttp3.RequestBody;
 
 
 @Controller
 public class UsersController {
 	@Autowired
 	UsersService service;
+	
+	@Autowired
+	SongService songService;
 
 	@PostMapping("/register")
 	public String addUser(@ModelAttribute Users user) {
@@ -28,23 +39,33 @@ public class UsersController {
 		} else {
 			System.out.println("User already exists");
 		}
-		return "home";
+		return "index";
 	}
 
 	@PostMapping("/validate")
 	public String validate(@RequestParam("email") String email,
-			@RequestParam("password") String password, HttpSession session)
+			@RequestParam("password") String password, HttpSession session, Model model)
 	{
-		if (service.validateUser(email, password) == true) {
+		
+		
+		if (service.validateUser(email, password) == true) 
+		{
 			String role = service.getRole(email);
 			session.setAttribute("email", email);
 			if (role.equals("admin")) {
 				return "adminHome";
-			} else {
+			}
+			else {
+				Users user = service.getUser(email);
+				boolean userStatus = user.isPremium();
+				List<Song> songsList = songService.fetchAllSongs();
+				model.addAttribute("songs", songsList);
+				model.addAttribute("isPremium", userStatus);
 				return "customerHome";
 			}
 
-		} else {
+		} 
+		else {
 			return "login";
 		}
 		
